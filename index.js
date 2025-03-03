dc237053b682bff3ec73cb2d8d8101e3fa7d99fb
Princess <prt898@usask.ca>
2025-03-02 22:44:25 -0600

    Adding completed website Header

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
