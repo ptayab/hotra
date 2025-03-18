4424867ea65523d3d83084045aaeb0612579e647
Princess <prt898@usask.ca>
2025-03-17 20:54:56 -0600

    Updated Header

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+    res.locals.activePage = req.path; // This will store the current route
+    next();
+});
+
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home", { activePage: "/" });
 })
 
 app.get("/about", (req, res) => {
-    res.render("about.ejs");
+    res.render("about", { activePage: "/about" });
 })
 
+app.get("/services", (req, res) => {
+    res.render("services", { activePage: "/services" });
+});
+
 app.get("/contact-us", (req, res) => {
-    res.render("contact.ejs");
+    res.render("contact.ejs",{ activePage: "/contact-us" });
 })
 
 
